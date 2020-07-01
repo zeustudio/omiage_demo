05fbf29ab6e547cccfddc1e9ec499b33adbac3cb
takurokuro <61910342+takurokuro@users.noreply.github.com>
2020-07-01 22:43:10 +0900

    Add files via upload

--- js/p5js.js
+++ js/p5js.js
@@ -1,84 +1,117 @@
-//p5js関係のコードはここからです．
+var pointCount = 300;
+var lissajousPoints = [];
+var lissajousPoints_works = [];
+var freqX = 10;
+var freqY = 10;
+var phi = -90;
 
-var x = 0.1, y = 0.1;
-var x2 = 0.1, y2 = 0.1;
-var x3 = 0.1, y3 = 0.1;
+var modFreqX = 1;
+var modFreqY = 2;
 
-// var a = -1.0, b = 0.05, c = 2.275,  d = -0.5;
-var a2 =  1.0, b2 = 0.0,  c2 = - 2.25, d2 = 0.2;
-var a3 =  1.0, b3 = 0.0,  c3 = - 1.9,  d3 = 0.4;
-var a = -1.0, b = 0.1,  c = 1.52,   d = -0.8;
-//var a = -1.0, b = 0.1,  c = 1.6,    d = -0.8;
-// var a3 =  2.0,  b3 = -0.2, c3 = - 1.75, d3 = 1.;
-// var a3 = -2.0, b3 = -0.05, c3 = 2.6, d3 = -0.5;
-// var a =  1.0,  b = -0.1, c = - 1.75, d = -1.;
+var lineWeight = 0.1;
+var lineColor;
+var lineAlpha = 100;
 
+var connectionRadius = 80;
+var connectionRamp = 6;
+
+var i1 = 0;
+var w1 = 0;
+var s = 0;
 
 function setup() {
   createCanvas(400, 400);
+  colorMode(RGB, 255, 255, 255, 100);
   blendMode(ADD);
-  background(0, 0, 0, 0);
-}
+  noFill();
+};
 
 function draw() {
-
-    // GIFアニメーションの位置指定
-  animationGif_transparent.position(0,0);
-
-  var _x, _y;
-  var A;
-
-  var _x2, _y2;
-  var A2;
-
-  var _x3, _y3;
-  var A3;
-
-  for (var i = 0; i < 100; i++) {
-
-    A = a * (x * x + y * y) + b * x * (x * x - 3 * y * y) + c;
-    _x = A * x + d * (x * x - y * y);
-    _y = A * y - 2 * d * x * y;
-    stroke(124, 155, 255, 50); //color
-    point(_x * 100 + width/2, - _y * 100 + height/2);
-
-    x = _x;
-    y = _y;
-
+  calculateLissajousPoints();
+  drawLissajous_intro();
+  if (i1 < pointCount) {
+    i1 += 1;
   }
 
-    for (var i2 = 0; i2 < 100; i2++) {
-
-    A2 = a2 * (x2 * x2 + y2 * y2) + b2 * x2 * (x2 * x2 - 3 * y2 * y2) + c2;
-    _x2 = A2 * x2 + d2 * (x2 * x2 - y2 * y2);
-    _y2 = A2 * y2 - 2 * d2 * x2 * y2;
-    stroke(0, 100, 20, 50); //color
-    point(_x2 * 100 + width/2, - _y2 * 100 + height/2);
-
-    x2 = _x2;
-    y2 = _y2;
-
-  }
-
-    for (var i3 = 0; i3 < 100; i3++) {
-
-    A3 = a3 * (x3 * x3 + y3 * y3) + b3 * x3 * (x3 * x3 - 3 * y3 * y3) + c3;
-    _x3 = A3 * x3 + d3 * (x3 * x3 - y3 * y3);
-    _y3 = A3 * y3 - 2 * d3 * x3 * y3;
-    stroke(200, 10, 80, 50); //color
-    point(_x3 * 100 + width/2, - _y3 * 100 + height/2);
-
-    x3 = _x3;
-    y3 = _y3;
+  if(s == 1){
+  calculateLissajousPoints_works();
+  drawLissajous_works();
+  if (w1 < pointCount) {
+    w1 += 1;
+    }
   }
 }
 
-
-let animationGif_transparent;;
-
-function preload() {
-    // GIFアニメーションのp5.jsイメージ
-    animationGif_transparent = createImg(src="./img/intro_black.gif");
+function calculateLissajousPoints() {
+  for (var i = 0; i <= pointCount; i++) {
+    var angle = map(i, 0, pointCount, 0, TAU);
+    var x = sin(angle * freqX + radians(phi)) * cos(angle * modFreqX);
+    var y = sin(angle * freqY) * cos(angle * modFreqY);
+    x *= width / 2 - 30;
+    y *= height / 2 - 30;
+    lissajousPoints[i] = createVector(x, y);
+  }
 }
 
-//p5js関係のコードはここまでです．
+function drawLissajous_intro() {
+  // p.background(255); //白背景で前回の描画図形を消す
+  background(0, 0, 0, 0); //重ねるために透明にする
+  lineColor = color(0, 0, 0); //イントロ曲線の色
+  strokeWeight(lineWeight);
+  push();
+  translate(width / 2, height / 2);
+  for (var i2 = 0; i2 < i1; i2++) {
+    var d = lissajousPoints[i1].dist(lissajousPoints[i2]);
+    var a = pow(1 / (d / connectionRadius + 1), 6);
+    if (d <= connectionRadius) {
+      stroke(lineColor, a * lineAlpha);
+      line(
+        lissajousPoints[i1].x,
+        lissajousPoints[i1].y,
+        lissajousPoints[i2].x,
+        lissajousPoints[i2].y
+      );
+    }
+  }
+  pop();
+}
+
+
+function calculateLissajousPoints_works() {
+  for (var i = 0; i <= pointCount; i++) {
+    var angle = map(i, 0, pointCount, 0, TAU);
+    var x = sin(angle * (freqX + 30) + radians(phi)) * cos(angle * modFreqX);
+    var y = sin(angle * freqY) * cos(angle * modFreqY);
+    x *= width / 2 - 30;
+    y *= height / 2 - 30;
+    lissajousPoints_works[i] = createVector(x, y);
+  }
+}
+
+function drawLissajous_works() {
+  background(0, 0, 0, 0); //重ねるために透明にする
+  lineColor_works = color(0, 50, 200); //作品の色
+  strokeWeight(lineWeight);
+  push();
+  translate(width / 2, height / 2);
+  for (var w2 = 0; w2 < w1; w2++) {
+    var d = lissajousPoints_works[w1].dist(lissajousPoints_works[w2]);
+    var a = pow(1 / (d / connectionRadius + 1), 6);
+    if (d <= connectionRadius) {
+      stroke(lineColor_works, a * lineAlpha);
+      line(
+        lissajousPoints_works[w1].x,
+        lissajousPoints_works[w1].y,
+        lissajousPoints_works[w2].x,
+        lissajousPoints_works[w2].y
+      );
+    }
+  }
+  pop();
+}
+
+function keyPressed() {
+  if (keyCode == ENTER) {
+    s += 1
+  }
+}
